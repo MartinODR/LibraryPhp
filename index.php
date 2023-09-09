17556b05a79ddd8bbf0902acdab085dd10dfdd30
MartinOdr <martinsteinturm@gmail.com>
2023-09-09 12:53:25 +0200

    main

--- index.php
+++ index.php
@@ -12,13 +12,39 @@
     
     <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
         <ul class="nav navbar-nav">
-            <li class="nav-item active">
-                <a class="nav-link" href="#">Nav 1 <span class="sr-only">(current)</span></a>
-            </li>
+
             <li class="nav-item">
-                <a class="nav-link" href="#">Nav 2</a>
+                <a class="nav-link" href="#">Webeando</a>
             </li>
+
+            <li class="nav-item">
+                <a class="nav-link" href="#">Home</a>
+            </li>
+ 
+            <li class="nav-item">
+                <a class="nav-link" href="#">Books</a>
+            </li>
+
+            <li class="nav-item">
+                <a class="nav-link" href="#">About Us</a>
+            </li>
+
         </ul>
     </nav>
+
+    <div class="container">
+        <div class="row"></div>
+
+            <div class="jumbotron">
+                <h1 class="display-3">Jumbo heading</h1>
+                <p class="lead">Jumbo helper text</p>
+                <hr class="my-2">
+                <p>More info</p>
+                <p class="lead">
+                    <a class="btn btn-primary btn-lg" href="Jumbo action link" role="button">Jumbo action name</a>
+                </p>
+            </div>
+
+    </div>
 </body>
 </html>
