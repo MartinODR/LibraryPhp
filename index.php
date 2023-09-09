e550b0810ef4c164dae92ac090ffc208bc6a952a
MartinOdr <martinsteinturm@gmail.com>
2023-09-09 13:43:22 +0200

    main

--- index.php
+++ index.php
@@ -1,39 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Sitio a Wevo</title>
-
-    <link rel="stylesheet" href="./css/bootstrap.min.css" />
-</head>
-<body>
-    Sitio a Wevo   
-    
-    <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
-        <ul class="nav navbar-nav">
-
-            <li class="nav-item">
-                <a class="nav-link" href="#">Webeando</a>
-            </li>
-
-            <li class="nav-item">
-                <a class="nav-link" href="#">Home</a>
-            </li>
- 
-            <li class="nav-item">
-                <a class="nav-link" href="#">Books</a>
-            </li>
-
-            <li class="nav-item">
-                <a class="nav-link" href="#">About Us</a>
-            </li>
-
-        </ul>
-    </nav>
-
-    <div class="container">
-        <div class="row"></div>
+<?php include("template/Header.php"); ?>
 
             <div class="jumbotron">
                 <h1 class="display-3">Jumbo heading</h1>
@@ -45,6 +10,4 @@
                 </p>
             </div>
 
-    </div>
-</body>
-</html>
+<?php include("template/Footer.php"); ?>
---
+++ template/Header.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sitio a Wevo</title>
+
+    <link rel="stylesheet" href="./css/bootstrap.min.css" />
+</head>
+<body>  
+    
+    <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+        <ul class="nav navbar-nav">
+
+            <li class="nav-item">
+                <a class="nav-link" href="#">Webeando</a>
+            </li>
+
+            <li class="nav-item">
+                <a class="nav-link" href="#">Home</a>
+            </li>
+ 
+            <li class="nav-item">
+                <a class="nav-link" href="#">Books</a>
+            </li>
+
+            <li class="nav-item">
+                <a class="nav-link" href="#">About Us</a>
+            </li>
+
+        </ul>
+    </nav>
+
+    <div class="container">
+        <div class="row">
+
+
